ecef3475080b55e8ea6c2e05afbdd64ef6a9f2eb
Evgeny Parkhomenko <77242330+Parxommm@users.noreply.github.com>
2026-03-08 19:19:22 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -45,7 +45,7 @@ async function bootstrap() {
   // Validation Pipeline settings
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
 
-  const PORT = app.get(ConfigService).get<number>('PORT') as number;
+  const PORT = Number(process.env.PORT) || app.get(ConfigService).get<number>('PORT') || 3000;
 
   // Swagger settings
   const config = new DocumentBuilder()
